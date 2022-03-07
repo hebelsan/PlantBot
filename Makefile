fb5bbe4070e7f9baac13b3486e32b8b65762bc3d
alex <vx228@uni-heidelberg.de>
2022-03-07 17:55:38 +0100

    minor adjustments

--- Makefile
+++ Makefile
@@ -7,10 +7,10 @@ install:
 	. env/bin/activate; pip install -Ur requirements.txt
 
 debug:
-	. env/bin/activate; FLASK_APP=app.py FLASK_ENV=development flask run
+	. env/bin/activate; FLASK_APP=app.py FLASK_ENV=development flask run --host=0.0.0.0 --port=8080
 
 production:
-	. env/bin/activate; FLASK_APP=app.py FLASK_ENV=production flask run
+	. env/bin/activate; FLASK_APP=app.py FLASK_ENV=production flask run --host=0.0.0.0 --port=8080
 
 clean:
 	rm -rf env
--- app.py
+++ app.py
@@ -14,14 +14,16 @@ def index():
 @app.route('/settings', methods=('GET', 'POST'))
 def create():
     if request.method == 'POST':
+        if request.form['relay_pin']:
+            pin = int(request.form['relay_pin'])
+            if pin != 0:
+                app.config['PUMP_RELAY_PIN'] = pin
         if request.form.get('pumping'):
             setPumping(True)
             app.config['IS_PUMPING'] = True
         else:
             setPumping(False)
             app.config['IS_PUMPING'] = False
-        if request.form['relay_pin']:
-            app.config['PUMP_RELAY_PIN'] = request.form['relay_pin']
         return redirect(url_for('index'))
 
     return render_template('settings.html')
@@ -32,7 +34,6 @@ def your_flask_route():
     return 'Sucesss', 200
 
 def setPumping(value):
-    print(value)
     app.config['IS_PUMPING'] = value
     if app.config['IS_PUMPING'] == False:
         stopPumping(app.config['PUMP_RELAY_PIN'])
